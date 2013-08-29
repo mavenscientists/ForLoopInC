1613d0eca0f6b66d39cf12b66d3c3c3369d37494
Karan <karan4c6@gmail.com>
2013-08-29 17:27:32 +0530

    for loop in c

---
+++ for.c
@@ -0,0 +1,42 @@
+  /**  
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    * 
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    * 
+    * Copyright 2013 Maven Scientists http://mavenscientists.com
+    */
+
+ /**
+   * @author	Maven Scientists (http://mavenscientists.com) 
+   * @email	info@mavenscientists.com
+   * ForLoop - This program describes the use of for loop
+   */
+
+#include <stdio.h>
+
+int main()
+{
+	int I;
+
+	for(I = 0; I < 10; I++) 
+	{
+		printf("%d\n", I);
+	}
+/*
+Syntax :
+for(initialize; condition; increment or decrement)
+{
+	// statements...
+	st1;
+	st2;
+}
+*/
+
+	return 0;
+}
